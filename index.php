c2b3492d82fe9a1972ad3ecae36f2d4e53ed4918
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-05 16:25:20 +0530

    index.php: remove modal div

--- index.php
+++ index.php
@@ -66,26 +66,6 @@
 		  </div>
 	</header>
 
-	<!-- Modal -->
-	<div class="modal fade" id="info-modal" role="dialog">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">Information</h4>
-				</div>
-				<div class="modal-body">
-					<p>
-						Successfully logged in / registered...
-					</p>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<div id="slider" data-section="home">
 		<div class="item">
 			<canvas id="sky"></canvas>

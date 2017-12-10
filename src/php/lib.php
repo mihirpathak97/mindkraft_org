c83c814441782ec841beafec122a377c0f607598
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-10 14:34:29 +0530

    lib: print PDO error message instead of returning

--- src/php/lib.php
+++ src/php/lib.php
@@ -33,7 +33,7 @@
   // PDO error
   function PDOerror($errorMessage)
   {
-    return "An SQL error occured! <br><br>Error message : $errorMessage";
+    echo "<b>An SQL error occured!</b> <br><br>Error message : $errorMessage";
   }
 
 ?>

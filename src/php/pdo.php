56529e9d1cbd5c3b1c7f9ae8cd5d4ed6507f5016
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-10 14:30:41 +0530

    use separate file to create PDO connection

---
+++ src/php/pdo.php
@@ -0,0 +1,22 @@
+<?php
+  // pdo.php - Initializes a basic PDO object
+  // Copyright (C) - Z-Coders
+
+  require 'sqlconf.php';
+
+  $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+
+  $options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false
+  ];
+
+  try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+  } catch (PDOException $e) {
+    if ($e->getCode() == 2002) {
+      echo "<b>Error!</b> <br><br>Server actively refused the SQL connection.";
+    }
+  }
+?>

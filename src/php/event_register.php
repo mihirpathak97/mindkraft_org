73c93d765fd38dcab380c2bc365937384902c9f7
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-10 17:35:38 +0530

    register: (wip) setup PDO transactions

--- src/php/event_register.php
+++ src/php/event_register.php
@@ -1,70 +1,51 @@
 <?php
-  $event = $_GET['event_id'];
-  $user = $_GET['user_id'];
-  if (strlen($user) != 16) {
+  $eventid = $_GET['event_id'];
+  $userid = $_GET['user_id'];
+  if (strlen($userid) != 16) {
     echo "Please log in to register!";
     return;
   }
-  if (strlen($user) == 16 && strlen($event) == 16) {
-    require '../php/sqlconf.php';
-    $con = mysqli_connect($host, $username, $password, $db);
-    if (check_exists_event($event)) {
-      $query = "select registered_users from ".$table_prefix."enduser_registration where event_id='".$event."'";
-      $record = mysqli_fetch_array(mysqli_query($con, $query), MYSQL_ASSOC);
-      if (!user_registered($user, $record)) {
-        $con->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-        $query = "update ".$table_prefix."enduser_registration set registered_users='".$record['registered_users'].$user.":'";
-        $result = mysqli_query($con, $query);
+  if (strlen($userid) == 16 && strlen($eventid) == 16) {
+    require 'pdo.php';
+    require_once 'sqlconf.php';
+    require_once 'lib.php';
+
+    global $pdo, $table_prefix;
+
+    // Begins SQL transaction
+    $pdo->beginTransaction();
+
+    // Check if event already exists in table
+    $query = "SELECT * FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    if ($stmt->rowCount() == 0){
+      $query = "INSERT INTO $table_prefix" . "event_registration VALUES ('$eventid','$userid')";
+    }
+    else {
+      // Get list of registered users
+      $query = $query = "SELECT registered_users FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
+      $stmt = $pdo->prepare($query);
+      $stmt->execute();
+      $registered_users = explode(':', $stmt->fetchColumn());
+
+      // Check if already registered
+      if (in_array($userid, $registered_users)) {
+        echo "You have already registered...";
+      }
+      else {
+        $registered_users = implode(':', $registered_users);
+        $query = "UPDATE $table_prefix" . "event_registration SET registered_users='$registered_users:$userid' WHERE event_id='$eventid'";
+        $stmt = $pdo->prepare($query);
+        $result = $stmt->execute();
         if ($result) {
-          echo "Succesfully registered for event!";
+          echo "You have been succesfully registered for this event!";
         }
-        else {
-          echo "There was an error registering for the event!";
-        }
-        $con->commit();
-        $con->close();
-      }
-      else {
-        echo "You have already registed for this event...";
       }
     }
-    else {
-      $con->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-      $query = "insert into ".$table_prefix."enduser_registration values ('".$event."','".$user.":')";
-      $result = $result = mysqli_query($con, $query);
-      if ($result) {
-        echo "Succesfully registered for event!";
-      }
-      else {
-        echo "There was an error registering for the event!";
-      }
-      $con->commit();
-      $con->close();
-    }
-  }
 
-  function check_exists_event($event_id)
-  {
-    require '../php/sqlconf.php';
-    $con = mysqli_connect($host, $username, $password, $db);
-    $query = "select * from ".$table_prefix."enduser_registration where event_id='".$event_id."'";
-    $result = mysqli_query($con, $query);
-    if (mysqli_num_rows($result) == 1) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
+    // Commit changes
+    $pdo->commit();
 
-  function user_registered($user_id, $record)
-  {
-    $records = explode(":", $record['registered_users']);
-    if (in_array($user_id, $records)) {
-      return true;
-    }
-    else {
-      return false;
-    }
   }
 ?>

2e43f32edc3fed7cd307a68c06563171ca320ee4
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-10 17:38:02 +0530

    register: rollback when exception occurs

--- src/php/event_register.php
+++ src/php/event_register.php
@@ -12,40 +12,46 @@
 
     global $pdo, $table_prefix;
 
-    // Begins SQL transaction
-    $pdo->beginTransaction();
+    try {
+      // Begins SQL transaction
+      $pdo->beginTransaction();
 
-    // Check if event already exists in table
-    $query = "SELECT * FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute();
-    if ($stmt->rowCount() == 0){
-      $query = "INSERT INTO $table_prefix" . "event_registration VALUES ('$eventid','$userid')";
-    }
-    else {
-      // Get list of registered users
-      $query = $query = "SELECT registered_users FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
+      // Check if event already exists in table
+      $query = "SELECT * FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
       $stmt = $pdo->prepare($query);
       $stmt->execute();
-      $registered_users = explode(':', $stmt->fetchColumn());
-
-      // Check if already registered
-      if (in_array($userid, $registered_users)) {
-        echo "You have already registered...";
+      if ($stmt->rowCount() == 0){
+        $query = "INSERT INTO $table_prefix" . "event_registration VALUES ('$eventid','$userid')";
       }
       else {
-        $registered_users = implode(':', $registered_users);
-        $query = "UPDATE $table_prefix" . "event_registration SET registered_users='$registered_users:$userid' WHERE event_id='$eventid'";
+        // Get list of registered users
+        $query = $query = "SELECT registered_users FROM $table_prefix" . "event_registration WHERE event_id='$eventid'";
         $stmt = $pdo->prepare($query);
-        $result = $stmt->execute();
-        if ($result) {
-          echo "You have been succesfully registered for this event!";
+        $stmt->execute();
+        $registered_users = explode(':', $stmt->fetchColumn());
+
+        // Check if already registered
+        if (in_array($userid, $registered_users)) {
+          echo "You have already registered...";
+        }
+        else {
+          $registered_users = implode(':', $registered_users);
+          $query = "UPDATE $table_prefix" . "event_registration SET registered_users='$registered_users:$userid' WHERE event_id='$eventid'";
+          $stmt = $pdo->prepare($query);
+          $result = $stmt->execute();
+          if ($result) {
+            echo "You have been succesfully registered for this event!";
+          }
         }
       }
-    }
 
-    // Commit changes
-    $pdo->commit();
+      // Commit changes
+      $pdo->commit();
+    } catch (PDOException $e) {
+      // Rollback if exception occurs
+      $pdo->rollback();
+      echo PDOerror($e->getMessage());
+    }
 
   }
 ?>

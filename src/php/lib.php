73828799426e1d2fa48fcafcd4d5e6b5be164764
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-10 14:47:42 +0530

    lib: add generateRandomString()

--- src/php/lib.php
+++ src/php/lib.php
@@ -5,29 +5,43 @@
    */
 
   const dept_list = array(
-                       'ae' => 'Aerospace Engineering',
-                       'bt' => 'Bio Technology',
-                       'bi' => 'Bio Informatics',
-                       'ce' => 'Civil Engineering',
-                       'cse' => 'Computer Science',
-                       'ece' => 'Electronics and Communication',
-                       'eee' => 'Electrical and Eclectronics',
-                       'eie' => 'Electronics and Instrumentation',
-                       'fp' => 'Foop Processing',
-                       'me' => 'Mechanical Engineering',
-                       'emt' => 'Media Technology',
-                       'nano' => 'Nano Technology',
-                       'snh' => 'Science and Humanities'
-                      );
-    const event_type = array(
-                        'tech' => 'Technical',
-                        'nontech' => 'Non Technical'
-                        );
-    const event_category = array(
-                            'event' => 'Event',
-                            'workshop' => 'Workshop',
-                            'game' => 'Games'
-                            );
+    'ae' => 'Aerospace Engineering',
+    'bt' => 'Bio Technology',
+    'bi' => 'Bio Informatics',
+    'ce' => 'Civil Engineering',
+    'cse' => 'Computer Science',
+    'ece' => 'Electronics and Communication',
+    'eee' => 'Electrical and Eclectronics',
+    'eie' => 'Electronics and Instrumentation',
+    'fp' => 'Foop Processing',
+    'me' => 'Mechanical Engineering',
+    'emt' => 'Media Technology',
+    'nano' => 'Nano Technology',
+    'snh' => 'Science and Humanities'
+  );
+
+  const event_type = array(
+    'tech' => 'Technical',
+    'nontech' => 'Non Technical'
+  );
+
+  const event_category = array(
+    'event' => 'Event',
+    'workshop' => 'Workshop',
+    'game' => 'Games'
+  );
+
+  // Generates a 16-digit alpha-numeric
+  function generateRandomString($length = 16) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+  }
 
 
   // PDO error

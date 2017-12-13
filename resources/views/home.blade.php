06e6a70d35bea3ac1a0ffd5eb8122615e58b29a4
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-13 20:52:42 +0530

    add dummy dates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,7 @@
         </svg>
       </nav> -->
       <img src="{{ URL::asset('images/mk.png') }}" alt="">
+      <h2 class="hero-date">14 March - 16 March</h2>
     </div>
 
     <div id="radial-menu" class="cm-container">

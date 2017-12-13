e2495dde8122fe1788dd120624cb1747d719e36f
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-14 00:33:55 +0530

    add template pages

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MindKraft 2018</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/radial-menu.css') }}">
+  </head>
+
+  <body>
+
+    <!-- Actual body -->
+
+    <!-- <div id="particle-canvas"></div> -->
+
+    <div id="base-hero" class="select-disable">
+      <!-- <nav>
+        <svg id="wave" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 38">
+          <path data-v-14b53e32="" data-name="Line 1" d="M0.91,15L0.78,15A1,1,0,0,0,0,16v6a1,1,0,1,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H0.91Z" class="line line-1"></path>
+          <path data-v-14b53e32="" data-name="Line 2" d="M6.91,15L6.78,9A1,1,0,0,0,6,10V28a1,1,0,1,0,2,0s0,0,0,0V10A1,1,0,0,0,7,9H6.91Z" class="line line-2"></path>
+          <path data-v-14b53e32="" data-name="Line 3" d="M12.91,15L12.78,0A1,1,0,0,0,12,1V37a1,1,0,1,0,2,0s0,0,0,0V1a1,1,0,0,0-1-1H12.91Z" class="line line-3"></path>
+          <path data-v-14b53e32="" data-name="Line 4" d="M18.91,15l-0.12,0A1,1,0,0,0,18,11V27a1,1,0,1,0,2,0s0,0,0,0V11a1,1,0,0,0-1-1H18.91Z" class="line line-4"></path>
+          <path data-v-14b53e32="" data-name="Line 5" d="M24.91,15l-0.12,0A1,1,0,0,0,24,16v6a1,1,0,0,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H24.91Z" class="line line-5"></path>
+        </svg>
+      </nav> -->
+      <br><br>
+      <h2 class="hero-head">Frequently Asked Questions</h2>
+    </div>
+
+    <div id="radial-menu" class="cm-container">
+      <ul class="cm-items"></ul>
+      <div class="cm-selected-container">
+        <div class="cm-selected-label">
+          <span><!-- Init here --></span>
+        </div>
+        <a class="cm-button cm-button-prev" type="button" title="Previous">&lt;</a>
+        <a class="cm-button cm-button-next" type="button" title="Next">&gt;</a>
+      </div>
+    </div>
+
+  </body>
+  <script src="{{ URL::asset('js/lodash.core.js') }}"></script>
+  <script src="{{ URL::asset('js/greensock/TweenMax.min.js') }}"></script>
+  <script src="{{ URL::asset('js/app.js') }}" charset="utf-8"></script>
+  <script src="{{ URL::asset('js/particles.js') }}" charset="utf-8"></script>
+</html>

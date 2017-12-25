3510a31207c55acbbc96d1cdcb270a05f1baa484
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-25 18:53:23 +0530

    migration: add views

---
+++ database/migrations/2017_12_25_131724_create_overall_views.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOverallViews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $view_prefix = env('DB_VIEW_PREFIX', '');
+        $table_prefix = env('DB_TABLE_PREFIX', '');
+        DB::statement("create view $view_prefix"."enduser as select * from $table_prefix"."enduser");
+        DB::statement("create view $view_prefix"."events_list as select * from $table_prefix"."events_list");
+        DB::statement("create view $view_prefix"."games_list as select * from $table_prefix"."games_list");
+        DB::statement("create view $view_prefix"."workshops_list as select * from $table_prefix"."workshops_list");
+        DB::statement("create view $view_prefix"."event_registration as select * from $table_prefix"."event_registration");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}

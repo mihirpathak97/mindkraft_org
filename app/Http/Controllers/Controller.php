9cfe4be2b8c88b6301cc668d3158933c7e97c8e7
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-25 21:40:40 +0530

    add random string generator to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Generates a 16-digit alpha-numeric
+    public function generateRandomString($length = 16) {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+      }
+
+      return $randomString;
+    }
+
 }

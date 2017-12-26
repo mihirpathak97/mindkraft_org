155283469d54243a5ed725280a78dd3ff43fe2f4
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-26 10:36:00 +0530

    migrate lib.php code to Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,36 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    // Departments list
+    const dept_list = array(
+      'ae' => 'Aerospace Engineering',
+      'bt' => 'Bio Technology',
+      'bi' => 'Bio Informatics',
+      'ce' => 'Civil Engineering',
+      'cse' => 'Computer Science',
+      'ece' => 'Electronics and Communication',
+      'eee' => 'Electrical and Eclectronics',
+      'eie' => 'Electronics and Instrumentation',
+      'fp' => 'Foop Processing',
+      'me' => 'Mechanical Engineering',
+      'emt' => 'Media Technology',
+      'nano' => 'Nano Technology',
+      'snh' => 'Science and Humanities'
+    );
+
+    // Event Types
+    const event_type = array(
+      'tech' => 'Technical',
+      'nontech' => 'Non Technical'
+    );
+
+    // Event Categories
+    const event_category = array(
+      'event' => 'Event',
+      'workshop' => 'Workshop',
+      'game' => 'Games'
+    );
+
     // Generates a 16-digit alpha-numeric
     public function generateRandomString($length = 16) {
       $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';

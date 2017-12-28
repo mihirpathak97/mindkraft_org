d20978fde6c6d408d766e4d3cd432cefa20c3859
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-28 17:49:26 +0530

    fix undefined var $type

--- app/Http/Controllers/EventInfoController.php
+++ app/Http/Controllers/EventInfoController.php
@@ -166,7 +166,7 @@ class EventInfoController extends Controller
         // else {
         //   // Check seats available
         // }
-        $acc = '<a href="/register/'.$type.'/'.$userid.'/'.$eventid.'">Register Now!</a>';
+        $acc = '<a href="/register/'.$path[count($path) - 3].'/'.$userid.'/'.$eventid.'">Register Now!</a>';
         return $acc;
       }
 

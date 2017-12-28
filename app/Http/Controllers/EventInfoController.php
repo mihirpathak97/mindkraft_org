6b8c3ee3f2546fa0ecaf2097a8ea308f9cbcb3c0
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-28 16:35:45 +0530

    add event info controller

---
+++ app/Http/Controllers/EventInfoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EventInfoController extends Controller
+{
+    public function getEventInfo(Request $request)
+    {
+
+      $prefix = env('DB_VIEW_PREFIX', '');
+
+      $path = explode('/', $request->path());
+      $id = $path[count($path) - 2];
+      $param = $path[count($path) - 1];
+
+      $event = DB::select('select * from '.$prefix.'events_list where id=\''.$id.'\'');
+      $event = $event[0];
+
+      if ($param == 'about') {
+        $acc = '{ "heading": "Info",';
+        $acc .= '"body": "'.$event->about.'"}';
+        return $acc;
+      }
+      if ($param == 'contact') {
+        $acc = '{ "heading": "Contact",';
+        $acc .= '"body": "'.$event->contact.'"}';
+        return $acc;
+      }
+      if ($param == 'fee') {
+        $acc = '{ "heading": "Fee",';
+        $acc .= '"body": "'.$event->fee.'"}';
+        return $acc;
+      }
+      if ($param == 'prize') {
+        $acc = '{ "heading": "Prize",';
+        $acc .= '"body": "'.$event->prize.'"}';
+        return $acc;
+      }
+
+      $acc = '{ "heading": "'.$param.'",';
+      $acc .= '"body": "Not Available"}';
+      return $acc;
+
+    }
+
+    public function prepareUserRegister(Request $request)
+    {
+
+      $prefix = env('DB_VIEW_PREFIX', '');
+
+      $path = explode('/', $request->path());
+      $userid = $path[count($path) - 2];
+      $eventid = $path[count($path) - 1];
+
+      if ($userid == 'nil') {
+        return "You Must Login first!";
+      }
+
+      else {
+        $user = DB::select('select * from '.$prefix.'enduser where id=\''.$userid.'\'');
+        $user = $user[0];
+        $acc = '<a href="/registerevent/'.$userid.'/'.$eventid.'">Register Now!</a>';
+        return $acc;
+      }
+
+    }
+}

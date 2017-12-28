15c9337a4f5f18801759b9b577257d6239c88c26
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-28 19:28:33 +0530

    login link fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -37,11 +37,11 @@
       <div id="navbar" class="navbar-collapse collapse enable-select">
         <ul class="nav-ul">
           <?php if (isset($username)) { ?>
-            <li><a href="user"><span><?php echo $username; ?></span></a></li>
-            <li><a href="logout"><span>Logout</span></a></li>
+            <li><a href="/user"><span><?php echo $username; ?></span></a></li>
+            <li><a href="/logout"><span>Logout</span></a></li>
           <?php }else{ ?>
-            <li><a href="login"><span>Login</span></a></li>
-            <li><a href="register"><span>Register</span></a></li>
+            <li><a href="/login"><span>Login</span></a></li>
+            <li><a href="/register"><span>Register</span></a></li>
           <?php } ?>
         </ul>
       </div>

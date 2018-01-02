58aea181fa029308b18a7313e5792e0c2aa61698
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-02 08:31:22 +0530

    admin table migrations

---
+++ database/migrations/2018_01_02_025833_create_admin_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_user', function (Blueprint $table) {
+            $table->string('username');
+            $table->binary('password');
+            $table->timestamp('timestamp');
+        });
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'admin\', password(\'Mi12971123581321\'))');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_user');
+    }
+}

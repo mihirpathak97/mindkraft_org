0114e9577c7f835ec7fbba571e8996c65af9fab8
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-10 23:48:40 +0530

    add default users for cpanel

--- database/migrations/2017_12_25_120745_create_cpanel_users_table.php
+++ database/migrations/2017_12_25_120745_create_cpanel_users_table.php
@@ -18,7 +18,9 @@ class CreateCpanelUsersTable extends Migration
             $table->binary('password');
             $table->timestamp('timestamp');
         });
-        DB::statement('insert into mindkraft18_cpanel_users (username, password) values (\'admin\', password(\'password\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'mihir\', password(\'ur15cs104\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'dalbut\', password(\'ur15mt058\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'vedha\', password(\'ur16mt019\'))');
     }
 
     /**

c6ed3ce71be5bc4ea9f70e8bb8a7fdaf444cccca
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-10 22:09:42 +0530

    add credentials for other admins

--- database/migrations/2018_01_02_025833_create_admin_user_table.php
+++ database/migrations/2018_01_02_025833_create_admin_user_table.php
@@ -18,7 +18,9 @@ class CreateAdminUserTable extends Migration
             $table->binary('password');
             $table->timestamp('date_created');
         });
-        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'admin\', password(\'Mi12971123581321\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'mihir\', password(\'ur15cs104\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'dalbut\', password(\'ur15mt058\'))');
+        DB::statement('insert into mindkraft18_admin_user (username, password) values (\'vedha\', password(\'ur16mt019\'))');
     }
 
     /**

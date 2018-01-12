95f0fdd60ac4e1fb775528b1505d08a647611099
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-12 10:21:47 +0530

    APIController: add more documentation

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,13 +10,16 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 |
 | Author - Mihir Pathak
-| Copyright (c) 2017 Z Coders All Rights Reserved
+| Copyright (c) 2018 Z Coders All Rights Reserved
 |
 | All calls except in auth group must contain a valid 64-digit alphanumeric
 | `api_token` (generated when a user registers). `api_token` is unique
 | and unchanging.
 |
-| Invalid `api_token` or improper API calls will return false
+| Invalid `api_token` or improper API calls will return false.
+|
+| [IMPORTANT] : `api_token` is not stored in the client-side in anyway. Use
+| `auth/get/api_token/` to retrieve it from the server.
 |
 */
 
@@ -33,7 +36,8 @@ class APIController extends Controller
   {
     /**
      * Accepts a mobile number and password
-     * Returns api_token
+     * Returns `api_token` and `userid`
+     * [NOTE] : `userid` can be stored on the client-side
      */
 
     return "true";
@@ -66,7 +70,7 @@ class APIController extends Controller
   {
     /**
      * Accepts mobile number, password and OTP
-     * Returns api_token
+     * Returns "You can now login"
      */
 
     return "true";

f362b2dee0d839d30a3d3bdca8c0fcd687df67ac
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-12 10:10:54 +0530

    APIController: Add documentation

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4,6 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/*
+|--------------------------------------------------------------------------
+| API Controller
+|--------------------------------------------------------------------------
+|
+| Author - Mihir Pathak
+| Copyright (c) 2017 Z Coders All Rights Reserved
+|
+| All calls except in auth group must contain a valid 64-digit alphanumeric
+| `api_token` (generated when a user registers). `api_token` is unique
+| and unchanging.
+|
+| Invalid `api_token` or improper API calls will return false
+|
+*/
+
 class APIController extends Controller
 {
 
@@ -15,21 +31,44 @@ class APIController extends Controller
 
   public function userAuthLogin(Request $request)
   {
+    /**
+     * Accepts a mobile number and password
+     * Returns api_token
+     */
+
     return "true";
   }
 
   public function userAuthRegister(Request $request)
   {
+    /**
+     * Accetps user registration data
+     * Returns "login successful" (until OTP framework is enabled)
+     * When OTP framework is enabled, will return a proper message
+     */
+
     return "true";
   }
 
   public function userAuthRegisterKarunya(Request $request)
   {
+    /**
+     * Accepts user registration data along with Karunya
+     * registration number
+     *
+     * Return - Same as userAuthRegister
+     */
+
     return "true";
   }
 
   public function userAuthAuthenticate(Request $request)
   {
+    /**
+     * Accepts mobile number, password and OTP
+     * Returns api_token
+     */
+
     return "true";
   }
 
@@ -38,25 +77,45 @@ class APIController extends Controller
   |--------------------------------------------------------------------------
   | Get Functions
   |--------------------------------------------------------------------------
+  |
+  | All calls require `api_token`
+  | All responses must be in strict JSON format
+  |
   */
 
   public function getEventsList(Request $request)
   {
+    /**
+     * Returns events list (departments)
+     */
+
     return "true";
   }
 
   public function getEventsListDepartment(Request $request)
   {
+    /**
+     * Returns department wise events list
+     */
+
     return "true";
   }
 
   public function getGamesList(Request $request)
   {
+    /**
+     * Returns games list
+     */
+
     return "true";
   }
 
   public function getWorkshopsList(Request $request)
   {
+    /**
+     * Returns workshops list
+     */
+
     return "true";
   }
 

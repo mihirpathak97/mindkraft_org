b58aed2222a94b0f9b7e9ae08a4e0d0fee8b2fb3
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-15 22:02:19 +0530

    add missing menu is req sub-class

--- resources/views/req/game.blade.php
+++ resources/views/req/game.blade.php
@@ -55,7 +55,23 @@
         <div class="modal animated fadeIn">
           <div class="modal-background"></div>
           <div class="modal-content">
-            <ol></ol>
+            <ol>
+							<li class="navbar-li"><a href="/home">Home</a></li>
+							<li class="navbar-li"><a href="/events">Events</a></li>
+							<li class="navbar-li"><a href="/workshops">Workshops</a></li>
+							<li class="navbar-li"><a href="/gmaes">Games</a></li>
+							<li class="navbar-li"><a href="/lectures">Lectures</a></li>
+							<li class="navbar-li"><a href="/exhibitions">Exhibitions</a></li>
+							<li class="navbar-li"><a href="/sponsors">Our Sponsors</a></li>
+							<li class="navbar-li"><a href="/contact">Contact</a></li>
+							<?php if (isset($username)): ?>
+								<li class="navbar-li"><a href="/user"><?php echo $username; ?></a></li>
+								<li class="navbar-li"><a href="/logout">Logout</a></li>
+							<?php else: ?>
+								<li class="navbar-li"><a href="/login">Login</a></li>
+								<li class="navbar-li"><a href="/register">Register</a></li>
+							<?php endif; ?>
+						</ol>
           </div>
           <button class="modal-close is-large" aria-label="close"></button>
         </div>

1b2c681638669995aa9f4ad2d26a148a662dd3f8
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-15 14:24:46 +0530

    rename migration class

--- database/migrations/2018_01_15_064528_add_to_overall_views.php
+++ database/migrations/2018_01_15_064528_add_to_overall_views.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateOverallViews extends Migration
+class AddToOverallViews extends Migration
 {
     /**
      * Run the migrations.

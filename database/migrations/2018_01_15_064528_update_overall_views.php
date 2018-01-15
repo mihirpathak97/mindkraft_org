8f77bff3e0f2928ccbbf5615b35eb02464c1082c
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-15 12:17:01 +0530

    add view for enduser_details

---
+++ database/migrations/2018_01_15_064528_update_overall_views.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOverallViews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      $view_prefix = env('DB_VIEW_PREFIX', '');
+      $table_prefix = env('DB_TABLE_PREFIX', '');
+      DB::statement("create view $view_prefix"."enduser_details as select * from $table_prefix"."enduser_details");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      $view_prefix = env('DB_VIEW_PREFIX', '');
+      DB::statement("drop view $view_prefix"."enduser_details");
+    }
+}

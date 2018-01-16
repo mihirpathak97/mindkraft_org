ed2c569085fe98bee21bb5d898093d22d5fd67df
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-16 14:49:41 +0530

    add "specific" definitions

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -348,4 +348,158 @@ class APIController extends Controller
 
   }
 
+  /*
+  |--------------------------------------------------------------------------
+  | Get Specific Functions
+  |--------------------------------------------------------------------------
+  |
+  | All calls require `api_token`
+  | Returns an event's details in JSON
+  |
+  */
+
+  public function getEventInfo(Request $request)
+  {
+    /**
+     * Accepts `event_id`
+     * and returns event info in JSON
+     */
+
+     $prefix = env('DB_TABLE_PREFIX', '');
+
+     $path = explode('/', $request->path());
+     $api_token = $path[count($path) - 4];
+     $id = $path[count($path) - 1];
+
+     if (!APIController::checkAPIToken($api_token)) {
+       return '{ "invalid_token": true }';
+     }
+
+     $query = 'SELECT * FROM '.$prefix.'events_list WHERE id= ? ';
+     $event = DB::select($query, [$id]);
+
+     if (count($event) != 1) {
+       return '{ "invalid_id": true }';
+     }
+
+     $event = $event[0];
+
+     $json_result = '{ "id": "'.$event->id.'", '.
+       '"name": "'.$event->name.'", '.
+       '"department": "'.Controller::dept_list[$event->department].'", '.
+       '"type": "'.Controller::event_type[$event->type].'", '.
+       '"contact": "'.$event->contact.'", '.
+       '"fee": "'.$event->fee.'", '.
+       '"prize": "'.$event->prize.'", '.
+       '"about": "'.$event->about.'", '.
+       '"seats": "'.$event->seats.'", ';
+
+       if ($event->open) {
+         $json_result .= '"is_open": true ';
+       }
+       else {
+         $json_result .= '"is_open": false ';
+       }
+
+     $json_result .= ' }';
+
+     return $json_result;
+
+  }
+
+
+  public function getGameInfo(Request $request)
+  {
+    /**
+     * Accepts `game_id`
+     * and returns game info in JSON
+     */
+
+     $prefix = env('DB_TABLE_PREFIX', '');
+
+     $path = explode('/', $request->path());
+     $api_token = $path[count($path) - 4];
+     $id = $path[count($path) - 1];
+
+     if (!APIController::checkAPIToken($api_token)) {
+       return '{ "invalid_token": true }';
+     }
+
+     $query = 'SELECT * FROM '.$prefix.'games_list WHERE id= ? ';
+     $game = DB::select($query, [$id]);
+
+     if (count($game) != 1) {
+       return '{ "invalid_id": true }';
+     }
+
+     $game = $game[0];
+
+     $json_result = '{ "id": "'.$game->id.'", '.
+       '"name": "'.$game->name.'", '.
+       '"contact": "'.$game->contact.'", '.
+       '"fee": "'.$game->fee.'", '.
+       '"prize": "'.$game->prize.'", '.
+       '"about": "'.$game->about.'", '.
+       '"seats": "'.$game->seats.'", ';
+
+       if ($game->open) {
+         $json_result .= '"is_open": true ';
+       }
+       else {
+         $json_result .= '"is_open": false ';
+       }
+
+     $json_result .= ' }';
+
+     return $json_result;
+
+  }
+
+  public function getWorkshopInfo(Request $request)
+  {
+    /**
+     * Accepts `workshop_id`
+     * and returns workshop info in JSON
+     */
+
+     $prefix = env('DB_TABLE_PREFIX', '');
+
+     $path = explode('/', $request->path());
+     $api_token = $path[count($path) - 4];
+     $id = $path[count($path) - 1];
+
+     if (!APIController::checkAPIToken($api_token)) {
+       return '{ "invalid_token": true }';
+     }
+
+     $query = 'SELECT * FROM '.$prefix.'workshops_list WHERE id= ? ';
+     $workshop = DB::select($query, [$id]);
+
+     if (count($workshop) != 1) {
+       return '{ "invalid_id": true }';
+     }
+
+     $workshop = $workshop[0];
+
+     $json_result = '{ "id": "'.$workshop->id.'", '.
+       '"name": "'.$workshop->name.'", '.
+       '"department": "'.$workshop->department.'", '.
+       '"contact": "'.$workshop->contact.'", '.
+       '"fee": "'.$workshop->fee.'", '.
+       '"about": "'.$workshop->about.'", '.
+       '"seats": "'.$workshop->seats.'", ';
+
+       if ($workshop->open) {
+         $json_result .= '"is_open": true ';
+       }
+       else {
+         $json_result .= '"is_open": false ';
+       }
+
+     $json_result .= ' }';
+
+     return $json_result;
+
+  }
+
 }

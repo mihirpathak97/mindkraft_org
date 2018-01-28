9c02e0e4ced05f8a186067b1a66708a8421296fc
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-28 14:53:12 +0530

    add inclutable template classes

---
+++ resources/views/includes/radialmenu.blade.php
@@ -0,0 +1,10 @@
+<div id="radial-menu" class="cm-container">
+  <ul class="cm-items"></ul>
+  <div class="cm-selected-container">
+    <div class="cm-selected-label">
+      <span><!-- Init here --></span>
+    </div>
+    <a class="cm-button cm-button-prev" type="button" title="Previous">&lt;</a>
+    <a class="cm-button cm-button-next" type="button" title="Next">&gt;</a>
+  </div>
+</div>

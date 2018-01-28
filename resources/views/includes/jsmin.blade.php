c6fddcbc06a0f8523cbc6b9966f3f32452b0e2a9
Mihir Pathak <mihirpathak97@gmail.com>
2018-01-28 14:56:48 +0530

    includes: add jsmin

---
+++ resources/views/includes/jsmin.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ URL::asset('js/lodash.core.js') }}"></script>
+<script src="{{ URL::asset('js/greensock/TweenMax.min.js') }}"></script>
+<script src="{{ URL::asset('js/app.js') }}" charset="utf-8"></script>

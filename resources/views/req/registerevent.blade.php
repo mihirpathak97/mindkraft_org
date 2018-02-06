4c461475fcd5db49b4c58089bc4c9d4e4d8873f6
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-06 16:24:31 +0530

    fix typo

--- resources/views/req/registerevent.blade.php
+++ resources/views/req/registerevent.blade.php
@@ -100,7 +100,7 @@
   </body>
 
   <script src="{{ URL::asset('js/lodash.core.js') }}"></script>
-  <script src="{{ URL::asset('js/weenMax.min.js') }}"></script>
+  <script src="{{ URL::asset('js/TweenMax.min.js') }}"></script>
   <script src="{{ URL::asset('js/app.js') }}" charset="utf-8"></script>
 
   <script type="text/javascript">

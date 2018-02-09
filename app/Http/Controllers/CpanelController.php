d271e67fd54961a57534bbd9f83a5bcc909f574f
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-09 22:18:52 +0530

    cpanel: create controller and add login logic

---
+++ app/Http/Controllers/CpanelController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CpanelController extends Controller
+{
+  // login logic
+  public function login(Request $request)
+  {
+    $prefix = env('DB_TABLE_PREFIX', '');
+
+    $username = $request->input('username');
+    $password = $request->input('password');
+
+    $query = 'SELECT * FROM '.$prefix.'cpanel_users WHERE username=? AND password=PASSWORD(?)';
+
+    $user = DB::select($query, [$username, $password]);
+
+    if (count($user) == 1) {
+      session([
+        'cpaneluser' => $user[0]->username,
+      ]);
+      return redirect('/cpanel/console');
+    }
+    else {
+      return redirect('cpanel');
+    }
+  }
+}

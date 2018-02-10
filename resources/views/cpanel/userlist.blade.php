631be1a460c1269dd2fecae84a27534875fe9b8c
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-10 10:48:23 +0530

    cpanel: finish user lists

---
+++ resources/views/cpanel/userlist.blade.php
@@ -0,0 +1,101 @@
+<?php
+  namespace App\Http\Controllers;
+  use URL, DB, Redirect;
+
+  if (!session()->has('cpaneluser') || !Controller::checkAdmin(session('cpaneluser'))) {
+    Redirect::to('cpanel')->send();
+  }
+
+  $prefix = env('DB_TABLE_PREFIX', '');
+  $query = 'SELECT * from '.$prefix.'enduser WHERE college=? order by name';
+  $list = DB::select($query, [$college]);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Admin Console</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/cms.css') }}">
+  </head>
+  <style media="screen">
+    .card{
+      margin: auto;
+      margin-top: 2rem;
+    }
+  </style>
+  <body>
+
+    <section class="hero is-primary">
+
+     <div class="hero-body" style="background:#383838">
+       <div class="container">
+         <div class="columns is-vcentered">
+           <div class="column">
+             <p class="title">
+               -$ DevConsole
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+
+     <div class="hero-foot">
+       <div class="container">
+         <nav class="tabs is-boxed">
+           <ul>
+             <li class="is-active">
+               <a href="/cpanel/console" id='active'>Admin Console</a>
+             </li>
+           </ul>
+         </nav></div>
+       </div>
+
+   </section>
+
+    <div id="app">
+
+      <nav class="navbar has-shadow">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item is-tab" href="/cpanel/console">Dashboard</a>
+            <a class="navbar-item is-tab is-active"><?php echo $college ?></a>
+          </div>
+        </div>
+      </nav>
+
+      <div class="box">
+        <h1><b>Statistics</b></h1>
+        <br><br>
+        <p>Total Users - <b><?php echo count($list) ?></b></p>
+        <br><br>
+      </div>
+
+      <table class="table card">
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Mobile</th>
+            <th>E-Mail</th>
+            <th>College</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($list as $record): ?>
+            <tr>
+              <td><?php echo $record->name; ?></td>
+              <td><?php echo $record->mobile; ?></td>
+              <td><?php echo $record->email; ?></td>
+              <td><?php echo $record->college; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+
+    </div>
+  </body>
+</html>

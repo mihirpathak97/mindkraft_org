76f9057173246099d9e9f164c4c3ea040c9f5cad
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-10 09:09:48 +0530

    migration: change class name entirely
    hope this works

--- database/migrations/2018_02_09_173853_set_username_unique_cpanel_users.php
+++ database/migrations/2018_02_09_173853_set_username_unique_cpanel_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCpanelUsers2 extends Migration
+class SetUsernameUniqueCpanelUsers extends Migration
 {
     /**
      * Run the migrations.

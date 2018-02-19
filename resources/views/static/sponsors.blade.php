559d497a1452f6c71c702cb89937ab279db8125d
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-19 18:56:56 +0530

    update sponsors

--- resources/views/static/sponsors.blade.php
+++ resources/views/static/sponsors.blade.php
@@ -3,39 +3,20 @@
 @section('title', 'Sponsors')
 
 <style media="screen">
-  .title-heading{
-    color: hsl(171, 100%, 41%) !important;
-    margin-bottom: 3rem !important;
-  }
-  .sponsor{
-  }
   .tmb{
     width: 200px;
     height: 120px;
+    padding: 0 10px;
   }
-  .cola{
-    width: 200px;
+  .paytm{
+    width: 300px;
     height: 200px;
     padding: 20px;
+    margin-top: -30px;
   }
   .syndicate{
     width: 300px;
     height: 150px;
-    padding: 20px;
-  }
-  .itc{
-    width: 180px;
-    height: 180px;
-    padding: 20px;
-  }
-  .accenture{
-    width: 280px;
-    height: 180px;
-    padding: 20px;
-  }
-  .hindu{
-    width: 280px;
-    height: 180px;
     padding: 20px;
   }
 </style>
@@ -44,14 +25,9 @@
 <div class="full-height">
   <div class="content">
     <div class="title">
-      <h2 class="title-heading">MindKraft 2017 Sponsors</h2>
-      <img src="{{ URL::asset('images/sponsors/old/tmb.png') }}" class="sponsor tmb" alt="TMB">
-      <img src="{{ URL::asset('images/sponsors/old/cola.png') }}" class="sponsor cola" alt="Coco-Cola">
-      <img src="{{ URL::asset('images/sponsors/old/syndicate.png') }}" class="sponsor syndicate" alt="Syndicate Bank">
-      <img src="{{ URL::asset('images/sponsors/old/itc.png') }}" class="sponsor itc" alt="ITC">
-      <br>
-      <img src="{{ URL::asset('images/sponsors/old/accenture.png') }}" class="sponsor accenture" alt="Accenture">
-      <img src="{{ URL::asset('images/sponsors/old/hindu.jpg') }}" class="sponsor hindu" alt="The Hindu">
+      <img src="{{ URL::asset('images/sponsors/paytm.png') }}" class="sponsor paytm" alt="PayTM">
+      <img src="{{ URL::asset('images/sponsors/tmb.png') }}" class="sponsor tmb" alt="TMB">
+      <img src="{{ URL::asset('images/sponsors/syndicate.png') }}" class="sponsor syndicate" alt="Syndicate Bank">
     </div>
   </div>
 </div>

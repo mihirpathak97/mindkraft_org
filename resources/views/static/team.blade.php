ae247dd3907092d994d2d51d4ded4c5d7de27399
Mihir Pathak <mihirpathak97@gmail.com>
2018-02-19 20:37:39 +0530

    new team layout

--- resources/views/static/team.blade.php
+++ resources/views/static/team.blade.php
@@ -14,6 +14,7 @@
     <title>MindKraft | Core Team</title>
 
     @include('includes.stylesheets')
+		<link rel="stylesheet" href="{{ URL::asset('css/team.css') }}">
   </head>
 
 	<style media="screen">
@@ -27,7 +28,7 @@
 
 		.content {
 			text-align: center;
-			width: 80%;
+			width: 100%;
 			display: block;
 			margin: auto;
 		}
@@ -76,7 +77,319 @@
 					</div>
 					<div class="students">
 						<h1>Student Co-Ordinators</h1>
-						<p class="gold">Renious Charles - MTECH IT</p>
+
+						<div class="main-cordinators">
+				      <div class="item">
+				        <figure>
+				          <img src="{{ URL::asset('images/profile/renious.jpg') }}">
+				          <div class="contact">
+				            <a href="https://twitter.com/ReniousCharles" class="tw"></a>
+				            <a href="https://www.facebook.com/renious.manleo" class="fb"></a>
+				            <a href="" class="gp"></a>
+				            <a href="" class="ma"></a>
+				          </div>
+				        </figure>
+				        <h1>Renious Charles<br>2nd MTECH FP</h1>
+				      </div>
+				      <div class="item">
+				        <figure>
+				          <img src="{{ URL::asset('images/profile/rizal.jpg') }}">
+				          <div class="contact">
+				            <a href="" class="tw"></a>
+				            <a href="" class="fb"></a>
+				            <a href="" class="gp"></a>
+				            <a href="" class="ma"></a>
+				          </div>
+				        </figure>
+				        <h1>Rizal Joseph<br>3rd ME</h1>
+				      </div>
+				      <div class="item">
+				        <figure>
+				          <img src="{{ URL::asset('images/profile/mobin.jpg') }}">
+				          <div class="contact">
+				            <a href="" class="tw"></a>
+				            <a href="" class="fb"></a>
+				            <a href="" class="gp"></a>
+				            <a href="" class="ma"></a>
+				          </div>
+				        </figure>
+				        <h1>Mobin Sam<br>3rd ME</h1>
+				      </div>
+				      <div class="item">
+				        <figure>
+				          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+				          <div class="contact">
+				            <a href="" class="tw"></a>
+				            <a href="" class="fb"></a>
+				            <a href="" class="gp"></a>
+				            <a href="mailto:vigneshl@karunya.edu.in" class="ma"></a>
+				          </div>
+				        </figure>
+				        <h1>Vignesh L<br>3rd EEE</h1>
+				      </div>
+				    </div>
+
+						<div class="promotion">
+							<h2><i class="fa fa-newspaper"></i>Promotion and Media</h2>
+							<div class="promotion-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Vignesh L<br>3rd EEE</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="exhibition">
+							<h2>Exhibitions</h2>
+							<div class="exhibition-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/mobin.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Mobin<br>3rd ME</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="workshop">
+							<h2>Workshop</h2>
+							<div class="workshop-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/renious.jpg') }}">
+					          <div class="contact">
+					            <a href="https://twitter.com/ReniousCharles" class="tw"></a>
+					            <a href="https://www.facebook.com/renious.manleo" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Renious<br>2nd MTECH FP<br>9787744647</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/akash.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Akash<br>2nd EEE<br>9500175255</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="tech">
+							<h2>Technical Events</h2>
+							<div class="tech-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Gideon<br>3rd BM<br>7708206236</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/rizal.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Rizal<br>3rd ME<br>8438153106</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="food">
+							<h2>Food Stalls</h2>
+							<div class="food-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Vignesh L<br>3rd EEE</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/mobin.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Mobin<br>3rd ME</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Gautham<br>3rd ME</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="decoration">
+							<h2>Decorations</h2>
+							<div class="decoration-inner">
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/subin.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Subin Sajan<br>3rd ME</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/kv.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Nivathitha<br>3rd CSE</h1>
+					      </div>
+								<div class="item">
+					        <figure>
+					          <img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+					          <div class="contact">
+					            <a href="" class="tw"></a>
+					            <a href="" class="fb"></a>
+					            <a href="" class="gp"></a>
+					            <a href="" class="ma"></a>
+					          </div>
+					        </figure>
+					        <h1>Ancy<br>3rd AE</h1>
+					      </div>
+							</div>
+						</div>
+
+						<div class="souvenir">
+							<h2>Souvenir</h2>
+							<div class="souvenir-inner">
+								<div class="item">
+									<figure>
+										<img src="{{ URL::asset('images/profile/alekhya.jpg') }}">
+										<div class="contact">
+											<a href="" class="tw"></a>
+											<a href="" class="fb"></a>
+											<a href="" class="gp"></a>
+											<a href="" class="ma"></a>
+										</div>
+									</figure>
+									<h1>Alekhya<br>2nd FP</h1>
+								</div>
+								<div class="item">
+									<figure>
+										<img src="{{ URL::asset('images/profile/wesley.jpg') }}">
+										<div class="contact">
+											<a href="" class="tw"></a>
+											<a href="" class="fb"></a>
+											<a href="" class="gp"></a>
+											<a href="" class="ma"></a>
+										</div>
+									</figure>
+									<h1>John Wesley<br>2nd EEE</h1>
+								</div>
+							</div>
+						</div>
+
+						<div class="design">
+							<h2>Design</h2>
+							<div class="design-inner">
+								<div class="item">
+									<figure>
+										<img src="{{ URL::asset('images/profile/anosh.jpg') }}">
+										<div class="contact">
+											<a href="" class="tw"></a>
+											<a href="" class="fb"></a>
+											<a href="" class="gp"></a>
+											<a href="" class="ma"></a>
+										</div>
+									</figure>
+									<h1>Anosh Xavier<br>3rd CSE</h1>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="sponsors">
+							<h2>Sponsorship Team</h2>
+							<div class="sponsors-inner">
+								<div class="item">
+									<figure>
+										<img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+										<div class="contact">
+											<a href="" class="tw"></a>
+											<a href="" class="fb"></a>
+											<a href="" class="gp"></a>
+											<a href="" class="ma"></a>
+										</div>
+									</figure>
+									<h1>Justin Varghese<br>4th ME</h1>
+								</div>
+								<div class="item">
+									<figure>
+										<img src="{{ URL::asset('images/profile/nobody.jpg') }}">
+										<div class="contact">
+											<a href="" class="tw"></a>
+											<a href="" class="fb"></a>
+											<a href="" class="gp"></a>
+											<a href="" class="ma"></a>
+										</div>
+									</figure>
+									<h1>Jerin John<br>4th ME</h1>
+								</div>
+							</div>
+						</div>
+
+
+						<!-- <p class="gold">Renious Charles - MTECH IT</p>
 						<p class="gold">Mobin Sam Baby - 3rd ME</p>
 						<p class="gold">Rizal Joseph - 3rd ME</p>
 						<p class="gold">Vignesh L - 3rd EEE</p>
@@ -98,7 +411,7 @@
 						<p>Stephan Raj - 3rd BT</p>
 						<p>Subin P Sajan - 3rd ME</p>
 						<p>Alekhya Alex - 2nd FP</p>
-						<p>Vetha Gnanam - 2nd EMT</p>
+						<p>Vetha Gnanam - 2nd EMT</p> -->
 					</div>
 				</div>
 			</div>
@@ -107,4 +420,33 @@
 
   </body>
   @include('includes.jsmin')
+	<script type="text/javascript">
+		// $('.promotion').click(function () {
+		// 	$('.promotion-inner').toggle();
+		// });
+		// $('.exhibition').click(function () {
+		// 	$('.exhibition-inner').toggle();
+		// });
+		// $('.workshop').click(function () {
+		// 	$('.workshop-inner').toggle();
+		// });
+		// $('.tech').click(function () {
+		// 	$('.tech-inner').toggle();
+		// });
+		// $('.food').click(function () {
+		// 	$('.food-inner').toggle();
+		// });
+		// $('.decoration').click(function () {
+		// 	$('.decoration-inner').toggle();
+		// });
+		// $('.souvenir').click(function () {
+		// 	$('.souvenir-inner').toggle();
+		// });
+		// $('.design').click(function () {
+		// 	$('.design-inner').toggle();
+		// });
+		// $('.sponsors').click(function () {
+		// 	$('.sponsors-inner').toggle();
+		// });
+	</script>
 </html>

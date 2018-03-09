51daf51c34cb66d33e85af8d2ec42f78106aed03
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-09 19:52:04 +0530

    cpanel + admin: cleanup

--- resources/views/cpanel/users.blade.php
+++ resources/views/cpanel/users.blade.php
@@ -70,7 +70,7 @@
 
       <?php if ($access_level == 1): ?>
         <div class="box">
-          <p><b>Total External Registrations</b> <?php echo count(DB::select('select * from mindkraft18_enduser where not college=\'Karunya Institute of Technology and Sciences, Coimbatore\'')); ?></p>
+          <p><b>Total External Registrations</b> - <?php echo count(DB::select('select * from mindkraft18_enduser where not college=\'Karunya Institute of Technology and Sciences, Coimbatore\'')); ?></p>
           <br><br>
           <?php
             foreach (Controller::colleges_list as $college):

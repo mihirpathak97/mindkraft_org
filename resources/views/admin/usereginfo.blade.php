df430125ccc0e8cc7e0a2bcd9066a982359c3e09
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-10 14:42:27 +0530

    admin: add jquery cdn

--- resources/views/admin/usereginfo.blade.php
+++ resources/views/admin/usereginfo.blade.php
@@ -120,6 +120,7 @@
 
     </div>
   </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
   <script type="text/javascript">
   $('#button').click(function () {
 

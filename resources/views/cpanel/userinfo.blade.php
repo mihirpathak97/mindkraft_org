9a8ad06377c6a09d30ea95f13425d3ae05e73073
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-13 14:20:53 +0530

    in words

--- resources/views/cpanel/userinfo.blade.php
+++ resources/views/cpanel/userinfo.blade.php
@@ -170,6 +170,7 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript">
+
         $('.checkbox').change(function () {
           if (this.checked) {
             $('#amt').text(parseInt($('#amt').text()) + parseInt(this.getAttribute("fee")));
@@ -200,6 +201,92 @@
               data = JSON.parse(data);
 
               var payed = {};
+
+              function numberToEnglish( n ) {
+
+                var string = n.toString(), units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words, and = 'and';
+
+                /* Is number zero? */
+                if( parseInt( string ) === 0 ) {
+                    return 'zero';
+                }
+
+                /* Array of units as words */
+                units = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+
+                /* Array of tens as words */
+                tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+
+                /* Array of scales as words */
+                scales = [ '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion' ];
+
+                /* Split user arguemnt into 3 digit chunks from right to left */
+                start = string.length;
+                chunks = [];
+                while( start > 0 ) {
+                    end = start;
+                    chunks.push( string.slice( ( start = Math.max( 0, start - 3 ) ), end ) );
+                }
+
+                /* Check if function has enough scale words to be able to stringify the user argument */
+                chunksLen = chunks.length;
+                if( chunksLen > scales.length ) {
+                    return '';
+                }
+
+                /* Stringify each integer in each chunk */
+                words = [];
+                for( i = 0; i < chunksLen; i++ ) {
+
+                    chunk = parseInt( chunks[i] );
+
+                    if( chunk ) {
+
+                        /* Split chunk into array of individual integers */
+                        ints = chunks[i].split( '' ).reverse().map( parseFloat );
+
+                        /* If tens integer is 1, i.e. 10, then add 10 to units integer */
+                        if( ints[1] === 1 ) {
+                            ints[0] += 10;
+                        }
+
+                        /* Add scale word if chunk is not zero and array item exists */
+                        if( ( word = scales[i] ) ) {
+                            words.push( word );
+                        }
+
+                        /* Add unit word if array item exists */
+                        if( ( word = units[ ints[0] ] ) ) {
+                            words.push( word );
+                        }
+
+                        /* Add tens word if array item exists */
+                        if( ( word = tens[ ints[1] ] ) ) {
+                            words.push( word );
+                        }
+
+                        /* Add 'and' string after units or tens integer if: */
+                        if( ints[0] || ints[1] ) {
+
+                            /* Chunk has a hundreds integer or chunk is the first of multiple chunks */
+                            if( ints[2] || ! i && chunksLen ) {
+                                words.push( and );
+                            }
+
+                        }
+
+                        /* Add hundreds word if array item exists */
+                        if( ( word = units[ ints[2] ] ) ) {
+                            words.push( word + ' hundred' );
+                        }
+
+                    }
+
+                }
+
+                return words.reverse().join( ' ' );
+
+              }
 
               function getEventName(key) {
                 var rsp = '';
@@ -238,6 +325,7 @@
                 }
               });
               printWindow.document.write('<br><b>Total</b>' + '<span style="float:right"> ₹ '+total+'</span>' + '<br>');
+              printWindow.document.write('<br><b>Total in Words</b>' + '<span style="margin-left: 30px"> ₹ '+numberToEnglish(total)+'</span>' + '<br>');
               printWindow.document.write('<br><br><br><br><b>Cashier</b>' + '<span style="float:right"><b>Organizing Secretary<br>MindKraft 2018</b></span>')
               printWindow.document.close(); // necessary for IE >= 10
               printWindow.focus(); // necessary for IE >= 10*/

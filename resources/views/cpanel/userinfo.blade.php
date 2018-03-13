b062aabaeabd9e27b54003c566dd24a436acc69f
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-13 14:10:01 +0530

    update

--- resources/views/cpanel/userinfo.blade.php
+++ resources/views/cpanel/userinfo.blade.php
@@ -223,8 +223,8 @@
 
               printWindow = window.open('', 'PRINT', 'height=400, width=600');
               printWindow.document.write('<html><head><br><br><title>MindKraft Registrtion Invoice</title><br><br>');
-              printWindow.document.write('<img src="https://mindkraft.org/images/mk-cropped.png" width="40px" height="40px" style="float:left; margin-left:20px; margin-top:20px">');
-              printWindow.document.write('<h2 align="center">MindKraft 2018</h2><br>');
+              printWindow.document.write('<img src="https://mindkraft.org/images/mk-cropped.png" width="40px" height="40px" style="float:left; margin-left:80px; margin-top:20px">');
+              printWindow.document.write('<h2>MindKraft 2018</h2><br>');
               printWindow.document.write('<b>Name: </b> - <?php echo $user->name ?><br>');
               printWindow.document.write('<b>User ID: </b> - <?php echo $user->id ?><br>');
               printWindow.document.write('<b>Receipt Number: </b> - ' + data.receipt + '<br>');

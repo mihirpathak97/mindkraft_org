81e952eb70a4449cd2b386f403a0230944ab008b
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-13 18:36:41 +0530

    team fix

--- resources/views/static/team.blade.php
+++ resources/views/static/team.blade.php
@@ -103,12 +103,6 @@
 				        </figure>
 				        <h1>Vignesh L<br>3rd EEE</h1>
 				      </div>
-							<div class="item">
-				        <figure>
-				          <img src="{{ URL::asset('images/profile/akash.jpg') }}">
-				        </figure>
-				        <h1>Akash<br>2nd EE</h1>
-				      </div>
 				    </div>
 
 						<div class="promotion">
@@ -121,6 +115,12 @@
 					        <h1>Vignesh L<br>3rd EEE</h1>
 					      </div>
 							</div>
+							<div class="item">
+				        <figure>
+				          <img src="{{ URL::asset('images/profile/akash.jpg') }}">
+				        </figure>
+				        <h1>Akash<br>2nd EE</h1>
+				      </div>
 						</div>
 
 						<div class="exhibition">

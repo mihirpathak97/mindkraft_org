072ab3ca95c38520c14005440b05dea0692683a5
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-15 01:37:35 +0530

    cpanel: fix invalid selector

--- resources/views/cpanel/accomodation.blade.php
+++ resources/views/cpanel/accomodation.blade.php
@@ -183,7 +183,7 @@
             type: 'POST',
             url: '/cpanel/user/<?php echo $user->id ?>/accomodation',
             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            data: {'from': $('#from').val(), 'to': $('to').val(), 'total': $('#total').val(), 'food': message},
+            data: {'from': $('#from').val(), 'to': $('#to').val(), 'total': $('#total').val(), 'food': message},
             success: function (data) {
 
               console.log(data);

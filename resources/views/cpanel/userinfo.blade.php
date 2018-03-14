a09e00dcebc5ef3204ba1374b6444b1c74aac396
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-14 18:09:26 +0530

    cpanel: disable level 10

--- resources/views/cpanel/userinfo.blade.php
+++ resources/views/cpanel/userinfo.blade.php
@@ -109,7 +109,7 @@
         </div>
       </nav>
 
-      <?php if ($access_level == 10): ?>
+      <?php if ($access_level == 99): ?>
         <div class="box">
           <p><b>Name</b> - <?php echo $user->name ?></p>
           <p><b>College</b> - <?php echo $user->college ?></p>

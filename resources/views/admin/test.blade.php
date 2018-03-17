428f5782f81d71b53f01c10d614a11d43a9cc6cc
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-17 11:14:43 +0530

    admin: test

--- resources/views/admin/test.blade.php
+++ resources/views/admin/test.blade.php
@@ -1,37 +1,60 @@
 <?php
-$prefix = env('DB_TABLE_PREFIX', '');
-$query = 'SELECT * from '.$prefix.'payment_info';
-$list = DB::select($query);
+// $prefix = env('DB_TABLE_PREFIX', '');
+// $query = 'SELECT * from '.$prefix.'payment_info';
+// $list = DB::select($query);
+//
+// $main_count = 0;
+// $main = 0;
+// $workshop = 0;
+//
+// function getWorkshopFee($id)
+// {
+//   if (count(DB::select('select * from mindkraft18_workshop_details where id=\''.$id.'\'')) > 0) {
+//     return DB::select('select * from mindkraft18_workshop_details where id=\''.$id.'\'')[0]->fee_external;
+//   }
+//   return 0;
+// }
+//
+// foreach ($list as $item) {
+//   $user = DB::select('select * from mindkraft18_enduser where id=\''.$item->id.'\'')[0];
+//   if ($user->college != 'Karunya Institute of Technology and Sciences, Coimbatore') {
+//     if (in_array('main', explode(':', $item->payed_for))) {
+//       $main += 300;
+//       $main_count++;
+//     }
+//     foreach (array_unique(explode(':', $item->payed_for)) as $workshop_id) {
+//       if ($workshop_id != 'main') {
+//         $workshop += getWorkshopFee($workshop_id);
+//       }
+//     }
+//   }
+// }
+//
+// echo $main_count . ' External Registrations<br>';
+// echo 'Main - '.$main . '<br>';
+// echo "Workshop - ".$workshop;
 
-$main_count = 0;
-$main = 0;
-$workshop = 0;
+$list = DB::select('select * from mindkraft18_approved_enduser');
 
-function getWorkshopFee($id)
-{
-  if (count(DB::select('select * from mindkraft18_workshop_details where id=\''.$id.'\'')) > 0) {
-    return DB::select('select * from mindkraft18_workshop_details where id=\''.$id.'\'')[0]->fee_external;
+$ku = 0;
+$ext = 0;
+$colleges = [];
+
+foreach ($list as $user) {
+  $user = DB::select('select * from mindkraft18_enduser where id=\''.$user.'\'')[0];
+  if ($user->college =='Karunya Institute of Technology and Sciences, Coimbatore') {
+    $ku++;
   }
-  return 0;
+  else {
+    $ext++;
+    array_push($college, $user->college);
+  }
 }
 
-foreach ($list as $item) {
-  $user = DB::select('select * from mindkraft18_enduser where id=\''.$item->id.'\'')[0];
-  if ($user->college != 'Karunya Institute of Technology and Sciences, Coimbatore') {
-    if (in_array('main', explode(':', $item->payed_for))) {
-      $main += 300;
-      $main_count++;
-    }
-    foreach (array_unique(explode(':', $item->payed_for)) as $workshop_id) {
-      if ($workshop_id != 'main') {
-        $workshop += getWorkshopFee($workshop_id);
-      }
-    }
-  }
-}
+echo 'Ku - ' . $ku . '<br>';
+echo 'Ext - ' . $ext . '<br>';
+echo 'colleges<br>';
+var_dump(array_unique($college));
 
-echo $main_count . ' External Registrations<br>';
-echo 'Main - '.$main . '<br>';
-echo "Workshop - ".$workshop;
 
 ?>

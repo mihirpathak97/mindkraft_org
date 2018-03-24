66cb88061ffd269ea71a72b0a6a5659aa4962681
Mihir Pathak <mihirpathak97@gmail.com>
2018-03-24 17:59:43 +0530

    migration: fix type error

--- database/migrations/2018_03_24_000100_create_enduser_details_table.php
+++ database/migrations/2018_03_24_000100_create_enduser_details_table.php
@@ -21,7 +21,7 @@ class CreateEnduserDetailsTable extends Migration
             $table->integer('year_of_study')->default(0);
             $table->string('field_of_study')->default('other');
             $table->integer('visit_count')->default(0);
-            $table->integer('mode_of_signup')->default('other');
+            $table->string('mode_of_signup')->default('other');
             $table->boolean('allow_sponsor_promo')->default(false);
             $table->boolean('require_accomodaiton')->default(false);
             $table->timestamps();

d33de7e7cca262bb0bf0503b68e71a6f7d5f1e0e
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-20 10:23:32 +0530

    add react components

---
+++ resources/assets/js/React.jsx
@@ -0,0 +1,25 @@
+/**
+ * React App entry point
+ */
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Views
+import Home from './views/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ resources/assets/js/views/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import {
+  Typography
+} from '@material-ui/core';
+
+class View extends Component {
+  render() {
+    return (
+      <div>
+        <Typography variant="title">MindKraft Boilerplate</Typography>
+      </div>
+    );
+  }
+}
+
+export default App;

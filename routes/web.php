0f52331af3a4a5be6438eaff1ed5f6c9fbc8ebf8
Mihir Pathak <mihirpathak97@gmail.com>
2018-07-24 15:49:26 +0530

    Clean up laravel routes

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,16 @@
 | Here is where you can register web routes for the application. These
 | routes are loaded by the Laravel backend
 |
+| NOTE: Please define your apps routes in 'resources/assets/js/React.jsx'
+| It is not advised to have any other routes here as they can interfere
+| with the React Router.
+|
 */
 
-
-// NOTE: Please define your apps routes in 'resources/assets/js/React.jsx'
+// Test Route
+// Uncomment this line if you want to test any code snippet
+// Route::view('/test', 'test'); 
 
 // 127.0.0.1
-Route::view('/', 'index');
+Route::view('/{path?}', 'index')
+      ->where('path', '.*');
